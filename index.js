3f2cf74ce46da1f37d4b9c272931afb4237177eb
Nestor Masinde <nestormasinde@gmail.com>
2024-01-07 20:12:38 +0300

    Sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const userId = data.id;
+        const element = document.createElement('p');
+        element.textContent = `New ID: ${userId}`;
+        document.body.appendChild(element)
+    })
+    .catch(error => {
+        const message = document.createElement('p');
+        message.textContent = `${error}`;
+        document.body.appendChild(message)
+    })
+
+}
+submitData("Steve", "steve@steve.com");
